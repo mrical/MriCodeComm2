0279a5e877e6738f857229ec462a8f5fd4524713
Mrical Singhal <mynameismric@gmail.com>
2020-10-07 22:08:01 +0530

    refactored sizebox component

--- src/components/DateBox.js
+++ src/components/DateBox.js
@@ -5,7 +5,7 @@ export default function DateBox({ date, dateType }) {
   return (
     <div className="my-2 pb-2" style={{ borderBottomWidth: "1px" }}>
       <div className="flex justify-around p-2 rounded-lg bg-gray-200">
-        <span>{dateType} At</span>
+        <span className="capitalize">{dateType} At</span>
         <span>{moment(date).format("hh:mm A D-MMM")}</span>
       </div>
     </div>
--- src/components/SizeBox.js
+++ src/components/SizeBox.js
@@ -7,7 +7,7 @@ export default function SizeBox({ size, bottomLine }) {
       style={{ borderBottomWidth: `${bottomLine ? "1px" : ""}` }}
     >
       <div className="flex justify-around p-2 rounded-lg bg-gray-200">
-        <span>size</span>
+        <span className="capitalize">size</span>
         <span className="uppercase">{size}</span>
       </div>
     </div>
