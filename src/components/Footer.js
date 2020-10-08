2c39159db4c645d25c6158919ec74a9a3ea45c1f
Mrical Singhal <mynameismric@gmail.com>
2020-10-08 10:29:43 +0530

    Footer and 404 page small tweaks 2

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,7 +1,13 @@
 import React from "react";
 import Link from "next/link";
 import categories from "../helpers/categories";
+import { adminSignOut } from "../actions/authActions";
+import { useRouter } from "next/router";
+import useAuth from "../hooks/useAuth";
 export default function Footer() {
+  const { authDispatch } = useAuth();
+  const router = useRouter();
+  const pathName = router.pathname.toLowerCase();
   return (
     <div className="w-full bg-purple-600 mt-5">
       <div className="w-11/12 mx-auto sm:w-5/6 lg:w-3/5 pt-4 pb-4 sm:pt-6 sm:pb-4 flex flex-col">
@@ -51,6 +57,24 @@ export default function Footer() {
                   All Products
                 </a>
               </Link>
+              {pathName.includes("/admin") && pathName !== "/admin/login" ? (
+                <button
+                  onClick={async () => {
+                    await adminSignOut(authDispatch);
+                    router.push("/");
+                  }}
+                >
+                  <div className="text-sm sm:text-lg hover:text-white sm:m-1 font-medium sm:font-semibold tracking-tight sm:tracking-normal">
+                    Admin Logout
+                  </div>
+                </button>
+              ) : (
+                <Link href="/admin">
+                  <a className="text-sm sm:text-lg hover:text-white sm:m-1 font-medium sm:font-semibold tracking-tight sm:tracking-normal">
+                    Admin
+                  </a>
+                </Link>
+              )}
             </div>
           </div>
         </div>
