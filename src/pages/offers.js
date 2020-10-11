8f46993f0b390f3b3241d0b4f775e4390d52e742
Mrical Singhal <mynameismric@gmail.com>
2020-10-11 12:36:53 +0530

    nav link active

--- src/pages/offers.js
+++ src/pages/offers.js
@@ -10,9 +10,9 @@ export default function offers() {
   return (
     <>
       <Head>
-        <title>Offers|MriCodecomm</title>
+        <title>Offers | MriCodecomm</title>
       </Head>
-      <DivideHeading title="Ofers" />
+      <DivideHeading title="Offers" />
       <div className="mx-auto grid p-1 sm:p-3 sm:w-full grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-3">
         {productsState?.length > 0 ? (
           featured.map((p) => (
@@ -22,7 +22,7 @@ export default function offers() {
           ))
         ) : productsState?.length === 0 ? (
           <div className="text-lg font-semibold text-center">
-            No Featured Products
+            No Offered Products
           </div>
         ) : (
           <ProductsLoader />
